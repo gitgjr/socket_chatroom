18dd82400fd7efb82ec89f5ccebd84f7346eaf2d
gitgjr <39017720+gitgjr@users.noreply.github.com>
2020-08-06 19:47:34 +0800

    json and new logic

--- socket.js
+++ socket.js
@@ -1,68 +1,140 @@
+//client
 $(function(){
     var socket = io();
-    var uname = null;//login name
-    var roomId = null;//roomId
+    var uname ;//login name
+    var password ;
+    var roomId ;//roomId
+
+    //login
     $('.login-btn').click(function(){
         uname = $.trim($('#loginName').val());
+        password = $.trim($('#password').val());
         if(uname){
-            socket.emit('login',{username:uname})
+            //showname();
+            socket.emit('login',{id:uname,password:password});
+            //alert("get successed");
         }else{
-            alert('please input nickname')
+            alert('please input your id')
         }
     });
-
-
-
-    socket.on('loginSuccess',function(data){
-        if(data.username === uname){
-            checkin1(data)
-        }else{
-            alert('user not match')
-        }
+    socket.on('same_id',function(){
+        alert('This ID has already logged in. Your can join another chat');
+        checkin1();
+    })
+    socket.on('loginSuccess',function(){
+        alert("welcome "+uname);
+        checkin1();
+        $('.logout-wrap').show('slow');
     });
 
     socket.on('loginFail',function(){
-        alert('same nick name')
+        alert('your id or password is wrong')
     });
 
-    socket.on('add',function(data){
-        checkin2();
-        var html = '<p>system message:  '+data.username+' joined the room</p>';
-        $('.chat-con').append(html);
+    //sign up
+    $('.go-signup').click(function (){
+        $('.login-wrap').hide('slow');
+        $('.signup-wrap').show('slow');
+    })
+    $('.signup-btn').click(function(){
+        uname = $.trim($('#new_loginName').val());
+        password = $.trim($('#new_password').val());
+        if(uname){
+            socket.emit('signup',{id:uname,password:password});
+        }else{
+            alert('please input your id')
+        }
+    });
+    socket.on('signupFail',function(){
+        alert('This ID has been registered')
+    })
+    socket.on('signupSuccess',function(){
+        alert('sign up success');
+        $('.signup-wrap').hide('slow');
+        $('.room-wrap').show('slow');
         showname();
+    })
+    //logout
+    $('.logout-btn').click(function(){
+        socket.emit('disconnect',{id:uname});
+    })
+    //add
+    socket.on('add',function(data){
+        var html = '<p>system message:  '+data.id+' joined the chat</p>';
+        $('.chat-con').append(html);
+        //showname();
     });
 
-    function checkin1(data){
+    function checkin1(){
         $('.login-wrap').hide('slow');
         $('.room-wrap').show('slow');
+        showname();
     }
-    function checkin2(data){
+    //room system
+    //join room
+    $('.room-btn').click(function(){
+        roomId = $.trim($('#roomId').val());
+        if(roomId){
+            socket.emit('join room',{roomId:roomId,id:uname})
+        }else{
+            alert('please input roomId')
+        }
+    });
+    socket.on('joinFail',function(){
+        alert('no such room id')
+    })
+    socket.on('joinSuccess',function (data){
+        alert('you joined '+data.roomId);
+        roomId=data.roomId;
+        $()
+        $('.room-name').html("<h3>"+"room id:"+roomId+"</h3>")
         $('.room-wrap').hide('slow');
         $('.chat-wrap').show('slow');
-    }
+        $('.quit').show('slow');
+    })
+    //pc
+    $('.pc-btn').click(function (){
+        var targetId;
+        targetId = $.trim($('#targetId').val());
+        if(targetId){
+            socket.emit('join pc',{targetId:targetId,id:uname})
+        }else{
+            alert('please input roomId')
+        }
+    })
+    socket.on('inviteFail',function(){
+        alert('no such user or he is outline')
+    })
+    //create room
+    $('.create-btn').click(function(){
+        socket.emit('create-room');
+    });
+    socket.on('create_success',function(data){
+        alert('created Successfully room id is'+data.roomId);
+        socket.emit('join room',{roomId:data.roomId,username:uname})
+    });
+    //
+    
+
     // leave
+    $('.quit-btn').click(function(){
+        //alert('click')
+        $('.room-wrap').show('slow');
+        $('.chat-wrap').hide('slow');
+        socket.emit('leave-room',{roomId:roomId,id:uname});
+    });
     socket.on('leave',function(name){
         if(name != null){
-            var html = '<p>system message:'+name+' has left chat room</p>';
+            var html = '<p>system message:'+name+'has leave</p>';
             $('.chat-con').append(html);
         }
     })
     function showname()
     {
-        $('.name-box').html("<h3>"+"online username:"+uname+"</h3>");
+        $('.name-box').html("<h3>"+"your id:"+uname+"</h3>");
     }
 
-    //room system
-    $('.room-btn').click(function(){
-        roomId = $.trim($('#roomId').val());
-        if(roomId){
-            socket.emit('join room',{roomId:roomId,username:uname})
-            $('.room-name').html("<h3>"+"chat room Id:"+roomId+"</h3>")
-        }else{
-            alert('please input roomId')
-        }
-    });
-    // send message
+    //message
     $('.sendBtn').click(function(){
         sendMessage()
     });
@@ -115,6 +187,17 @@ $(function(){
         if(data.state==0){
             $('.typing-box').hide('slow');
         }
+    })
+    // show list
+    $('.show-btn').click(function(){
+        socket.emit('show-list');
+        $('.live_list').html(' ');
+    })
+    socket.on('onlineid',function(data){
+        $('.live_list').append('<p>'+data.id+' online</p>');
+    })
+    socket.on('offlineid',function(data){
+        $('.live_list').append('<p>'+data.id+' offline</p>');
     })
 
 })
